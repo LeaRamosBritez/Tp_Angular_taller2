f7b730f8ffc347e41bcba6b4be3dc8b0d03cbaaf
LeaRamos <108842558+LeaRamosBritez@users.noreply.github.com>
2022-11-08 13:37:33 -0300

    services de usuario

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Iuser } from '../models/iuser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private baseUrl = 'http://localhost:3050';
+
+  constructor(private httpClient: HttpClient) { }
+
+  registrarUsuario(usuario: Iuser): any {
+    return this.httpClient.post(`${this.baseUrl}/registrarUsuario`, usuario);
+  }
+
+  loginUsuario(usuario: Iuser): any {
+    return this.httpClient.post(`${this.baseUrl}/login`, usuario);
+  }
+
+  usuarioActual(){
+    return this.httpClient.get(`${this.baseUrl}/usuarioActual`);
+  }
+
+  logout(): any {
+    return this.httpClient.post(`${this.baseUrl}/logout`,null);
+  }
+}
